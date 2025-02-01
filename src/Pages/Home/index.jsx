07cb3596f156c49919fb0370d650ea0c783105d6
Angie Natalia Mojica <100453879+An6ie02@users.noreply.github.com>
2025-02-01 11:28:24 -0500

    Card Component

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react"
 import Layout from "../../Components/Layout"
+import Card from "../../Components/Card"
 
 function Home() {
+  const [items, setItems] = useState(null)
 
   return (
       <Layout>
         React Home
+        <Card />
       </Layout>
   )
 }
