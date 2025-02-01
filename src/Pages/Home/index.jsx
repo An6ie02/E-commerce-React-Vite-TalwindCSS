29866d07de1faa082bfc69dff2ab533ebb9e59fa
Angie Natalia Mojica <100453879+An6ie02@users.noreply.github.com>
2025-02-01 12:05:40 -0500

    API Connection

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,15 +1,34 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Layout from "../../Components/Layout"
 import Card from "../../Components/Card"
+import { apiUrl } from "../../api"
 
 function Home() {
   const [items, setItems] = useState(null)
 
+  useEffect(() => {
+    fetch(`${apiUrl}/products`)
+      .then((response) => response.json())
+      .then((data) => {
+        setItems(data)
+      })
+      .catch((error) => {
+        console.error('Error:', error)
+      })
+  }, [])
+
   return (
-      <Layout>
-        React Home
-        <Card />
-      </Layout>
+    <Layout>
+      React Home
+      <div className="grid gap-6 grid-cols-4 w-full max-w-screen-lg">
+        {
+          items?.map(item => (
+            <Card key={item.id} props={item} />
+          ))
+        }
+
+      </div>
+    </Layout>
   )
 }
 
