8167253fa286e9ec1e16199f24de0b0ce83edc85
Angie Natalia Mojica <100453879+An6ie02@users.noreply.github.com>
2025-02-03 19:26:13 -0500

    Product Detail

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -3,12 +3,27 @@ import { createContext, useState } from 'react'
 export const ShoppingCartContext = createContext()
 
 export const ShoppingCartProvider = ({ children }) => {
+  // Shopping Cart - Count
   const [count, setCount] = useState(0)
+
+  // Product Detail - Open/Close
+  const [isOpenDetail, setIsOpenDetail] = useState(false)
+  const openProductDetail = () => setIsOpenDetail(true)
+  const closeProductDetail = () => setIsOpenDetail(false)
+
+  // Product Detail - Show
+  const [productToShow, setProductToShow] = useState({})
+
 
   return (
     <ShoppingCartContext.Provider value={{
       count,
-      setCount
+      setCount,
+      isOpenDetail,
+      openProductDetail,
+      closeProductDetail,
+      productToShow,
+      setProductToShow
     }}>
       {children}
     </ShoppingCartContext.Provider>
